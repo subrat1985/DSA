a43b8165612ead1e4fc1da87710df5ded678ae33
subrat1985 <subrat.pintu1@gmail.com>
2022-06-12 06:43:20 +0530

    Binary search added

---
+++ com/subrat/array/BinarySc.java
@@ -0,0 +1,36 @@
+package com.subrat.array;
+
+import java.util.Scanner;
+
+/*
+Searching a element in sorted array using linear search.
+ */
+public class BinarySc {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int k= sc.nextInt();
+        int[] arr= new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+        int result=binSearch(arr,k);
+        System.out.println("Binary search result  "+result);
+    }
+
+    private static int binSearch(int[] arr, int k) {
+        int start=0;
+        int end=arr.length-1;
+        while (start<=end){
+            int mid=start+((end-start)/2);
+            if (arr[mid]==k){
+                return mid;
+            }else if(arr[mid]>k){
+                end=mid-1;
+            }else {
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+}
