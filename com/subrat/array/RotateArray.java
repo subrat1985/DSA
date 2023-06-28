2f978da1396b917500df00c06ea95c4e614ca0c3
subrat kumar Mishra <subrat.pintu1@gmail.com>
2023-06-28 09:40:17 +0530

    rotate array clock wise

---
+++ com/subrat/array/RotateArray.java
@@ -0,0 +1,30 @@
+package com.subrat.array;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        int d=3;
+        int n=arr.length;
+        rotate(arr,d,n);
+
+    }
+    static void rotate(int arr[],int d,int n){
+        int rot=d%n;
+        int temp[]=new int[rot];
+        int j=0;
+        for(int i=0;i<rot;i++){
+            temp[i]=arr[j++];
+        }
+        for(int i=0;i<n-rot;i++){
+            arr[i]=arr[i+rot];
+        }
+        int k=0;
+        for(int i=n-rot;i<n;i++){
+            arr[i]=temp[k++];
+        }
+        for (int i=0;i<n;i++){
+            System.out.print(arr[i]+",");
+        }
+
+    }
+}
