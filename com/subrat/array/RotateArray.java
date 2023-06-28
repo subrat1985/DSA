d5559b6204a70b4adb4c9875ce9ab2696e6e1977
subrat kumar Mishra <subrat.pintu1@gmail.com>
2023-06-28 09:48:02 +0530

    link added

--- com/subrat/array/RotateArray.java
+++ com/subrat/array/RotateArray.java
@@ -1,5 +1,5 @@
 package com.subrat.array;
-
+//https://practice.geeksforgeeks.org/problems/rotate-array-by-n-elements-1587115621/1
 public class RotateArray {
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5};
